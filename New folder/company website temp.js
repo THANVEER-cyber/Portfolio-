57e43f516670930cb3ef76141c9c26d34073e36f
THANVEER-cyber <thannuthannu0141@gmail.com>
2025-03-26 13:36:22 +0530

    Delete New folder directory

--- New folder/company website temp.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('header nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        target.scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
--- New folder/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-
-// Example of handling form submission in contact page
-document.querySelector('.contact-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Thank you for reaching out! I will get back to you soon.');
-});
